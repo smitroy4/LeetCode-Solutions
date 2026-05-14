835d2f6a956f12c272cf444df880101d889359ee
Smit Roy <ttssmitherio@gmail.com>
2026-05-14 22:58:00 +0530

    Time: 2 ms (17.95%) | Memory: 42.9 MB (95.57%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,7 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
-                String prefix = strs[0];
+        String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
             int j = 0;
             while (j < Math.min(prefix.length(), strs[i].length())) {
@@ -13,6 +13,6 @@ class Solution {
             prefix = prefix.substring(0, j);
         }
         return prefix;
-        
+
     }
 }
