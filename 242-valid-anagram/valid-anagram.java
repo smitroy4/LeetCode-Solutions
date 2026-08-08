ee2fdb5a66b2f3a8dd1f2128b28ce55d2d1046b4
Smit Roy <smitroy22@gmail.com>
2026-08-08 11:34:51 +0530

    Time: 4 ms (82.85%) | Memory: 46.4 MB (43.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,29 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        //first check!
         if(s.length() != t.length()){
             return false;
         }
 
-        //created a int[]
-        int count[] = new int[26];
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
 
-        //lopped through the strings to count the character count
-        for(int i = 0; i< s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
 
-        // another loop to iterate over the count to check whether they're zero
-        for(int val : count){
-            if (val != 0){
-                return false;
-            }
-        }
-
-        // finally returned true
-        return true;
+        return Arrays.equals(sSort,tSort);
         
     }
 }
